552c8beb6b76910aa373430e0129842a8ba03304
Fantôme Beignet <himself@fantomebeig.net>
2023-03-10 10:26:33 +0100

    better aliases

--- init.zsh
+++ init.zsh
@@ -5,11 +5,15 @@ fi
 
 export EXA_COLORS='da=1;34:gm=1;34'
 
+export EXA_ICON_SPACING=2
+
 alias ls='exa --group-directories-first'
 alias ll='ls -l --git'        # Long format, git status
 alias l='ll -a'               # Long format, all files
-alias lr='ll -T'              # Long format, recursive as a tree
-alias lx='ll -sextension'     # Long format, sort by extension
-alias lk='ll -ssize'          # Long format, largest file size last
-alias lt='ll -smodified'      # Long format, newest modification time last
-alias lc='ll -schanged'       # Long format, newest status change (ctime) last
+alias la='ll -F --icons'      # Long format, with icons and file type indicators
+alias lg='la --git-ignore'    # la with respect to .gitignore
+alias lr='la -T'              # Long format, recursive as a tree
+alias lx='la -sextension'     # Long format, sort by extension
+alias lk='la -ssize'          # Long format, largest file size last
+alias lt='la -smodified'      # Long format, newest modification time last
+alias lc='la -schanged'       # Long format, newest status change (ctime) last
